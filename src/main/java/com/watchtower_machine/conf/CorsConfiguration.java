39a9e814e1b5c3673e1bd915b105e3345b5f56bb
Quentin D.C <quent043@hotmail.com>
2020-08-30 13:11:53 +0200

    Added httpClient eapache dependency to the pom.xml
    Added a Spring Configuration class "CorsConfigurer" Overriding the "addCorsMappings()" method to allog CORS policy in the API. ---> Was not possible to use several LocalHost origins before that.

---
+++ src/main/java/com/watchtower_machine/conf/CorsConfiguration.java
@@ -0,0 +1,26 @@
+package com.watchtower_machine.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration
+{
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+//                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+//                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+//                registry.addMapping("/**").allowedOrigins("http://magicseaweed.com");
+
+            }
+        };
+    }
+}
