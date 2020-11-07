22d6868615014d1a29616bdd8eea900ca87b44c0
Quentin D.C <quent043@hotmail.com>
2020-11-07 18:52:41 +0100

    Initiating an Aspect Oriented Logger .

---
+++ src/main/java/com/watchtower_machine/aop/LoggerAspect.java
@@ -0,0 +1,13 @@
+package com.watchtower_machine.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggerAspect {
+//private static final Logger LOGGER = LoggerFactory.getLogger();
+
+}
